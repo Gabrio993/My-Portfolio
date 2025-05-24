630080fe8250bf0cbc4b652ec378199982dd312a
gabrio993 <gabriele93.fusco@gmail.com>
2025-05-24 14:26:19 +0200

    feat(app / main) implemented dynamic routing between pages in app.tsx  and rendered in main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
-import React from "react";
-import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import NotFound from "./pages/notFound";
+import Index from "./pages";
 
 const App = () => {
-  return <h1>Hello, React with TypeScript!</h1>;
+  return (
+    // Aggiungi il return per restituire il JSX
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
