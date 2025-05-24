b1fba259081f25345d3013d595b50944f8a11121
gabrio993 <gabriele93.fusco@gmail.com>
2025-05-24 16:09:16 +0200

    style(components) fixed hero section  min-h

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,7 +9,7 @@ const Hero = () => {
   };
 
   return (
-    <div className="min-h-[75vh] flex flex-col justify-center pt-16">
+    <div className="min-h-[80vh]  flex flex-col justify-center ">
       <div className="max-w-3xl">
         <h1 className="text-5xl sm:text-6xl md:text-7xl font-bold mb-6">Hello, I'm a Web Developer</h1>
         <p className="text-xl md:text-2xl text-gray-700 mb-10 max-w-2xl">
