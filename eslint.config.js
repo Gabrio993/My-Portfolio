2305e68dd380926a42ab24aa3b69c480cb9f0c05
gabrio993 <gabriele93.fusco@gmail.com>
2025-05-24 14:12:48 +0200

    feat(config) defined and update tailwind, ts, eslint and post css configuration

--- eslint.config.js
+++ eslint.config.js
@@ -2,34 +2,24 @@ import js from "@eslint/js";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
-import ts from "@typescript-eslint/eslint-plugin";
-import tsParser from "@typescript-eslint/parser";
+import tseslint from "typescript-eslint";
 
-export default [
+export default tseslint.config(
   { ignores: ["dist"] },
   {
-    files: ["**/*.{js,jsx,ts,tsx}"],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
-      parser: tsParser, // Usa il parser TypeScript
-      parserOptions: {
-        ecmaVersion: "latest",
-        ecmaFeatures: { jsx: true },
-        sourceType: "module",
-      },
     },
     plugins: {
-      "@typescript-eslint": ts, // Aggiunge il plugin per TypeScript
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
     },
     rules: {
-      ...js.configs.recommended.rules,
-      ...ts.configs.recommended.rules, // Aggiunge le regole consigliate per TypeScript
       ...reactHooks.configs.recommended.rules,
-      "no-unused-vars": ["error", { varsIgnorePattern: "^[A-Z_]" }],
       "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
     },
-  },
-];
+  }
+);
