14164d9eb506b414cf4c62d856e8aa962f67aa4f
gabrio993 <gabriele93.fusco@gmail.com>
2025-06-05 20:38:23 +0200

    fix(config) client side public email configuration

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,4 @@
-export const PUBLIC_CONTACT_EMAIL = "gabriele93.fusco@gmail.com";
+const localPart = "gabriele93.fusco";
+const domain = "gmail.com";
+
+export const PUBLIC_CONTACT_EMAIL = `${localPart}@${domain}`;
