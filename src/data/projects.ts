134fb4479e23f76b84ba4c1a9af4b490055d9e7a
gabrio993 <gabriele93.fusco@gmail.com>
2026-04-28 20:34:36 +0200

    fix: ui data visualization in cards

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -64,7 +64,7 @@ const projects: Project[] = [
     description:
       "Project management web application built with PHP and CodeIgniter. It allows creating and managing clients, projects, and users with authentication, assigning projects to users, and tracking hours worked on each project. The system also includes reporting features and Excel export for data analysis.",
     githubUrl: "https://github.com/Gabrio993/Documentazione-Nuxt",
-    technologies: ["Php, CodeIgniter, Javascript"],
+    technologies: ["Php", "CodeIgniter", "Javascript"],
   },
 ];
 
